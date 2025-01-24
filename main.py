992318c1faa509ab9a105f1c3540155ae0f64b1a
gala999tool <gala.bombilo999@gmail.com>
2025-01-24 19:02:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] ACCOUNT PASSWORD[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] ACCESS KEY[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] TRYING TO LOGIN[/bold cyan]: ", end=None)
-        cpm = CPMEwan1999(acc_access_key)
+        cpm = CarParkTool(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
